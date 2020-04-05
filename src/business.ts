879a4d730530992156a476fc977697d67c65bef3
kaddo <kofi.addo@facile.it>
2020-04-05 14:05:31 +0200

    biz and sportuk

---
+++ src/business.ts
@@ -0,0 +1,28 @@
+import { News } from './Interfaces/INews';
+import axios from 'axios';
+import dotenv from 'dotenv';
+import Telegraf from 'telegraf';
+
+dotenv.config();
+
+export const send = async (): Promise<void> => {
+  const apiClient = axios.create({
+    baseURL: 'http://newsapi.org/v2/',
+    responseType: 'json',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const response = await apiClient.get<News>(
+    `top-headlines?category=business&pageSize=5&apiKey=${process.env.NEWS_API}`,
+  );
+
+  const bot = new Telegraf(process.env.BOT_TOKEN);
+  const articleToSend = response.data.articles.map((article) =>
+    bot.telegram.sendMessage(
+      process.env.CHAT_ID,
+      `${article.title}: \n ${article.url}`,
+    ),
+  );
+  await Promise.all(articleToSend);
+};
