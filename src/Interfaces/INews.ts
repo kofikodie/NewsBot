3696aed3362fbb024b648020237c4ccea35c1c7b
kaddo <kofi.addo@facile.it>
2020-04-05 13:03:25 +0200

    feat: headlines and sportitalia

---
+++ src/Interfaces/INews.ts
@@ -0,0 +1,19 @@
+export interface News {
+    status: string;
+    totalResults: number;
+    articles?: Array<ArticlesEntity> | null;
+}
+export interface ArticlesEntity {
+    source: Source;
+    author?: string | null;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage?: string | null;
+    publishedAt: string;
+    content: string;
+}
+export interface Source {
+    id?: null;
+    name: string;
+}
