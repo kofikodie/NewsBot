176f96a5c2cedbdee783668925f7284459b19371
kaddo <kofi.addo@facile.it>
2020-04-05 20:09:37 +0200

    feat: lint

--- src/Interfaces/INews.ts
+++ src/Interfaces/INews.ts
@@ -1,19 +1,19 @@
 export interface News {
-    status: string;
-    totalResults: number;
-    articles?: Array<ArticlesEntity> | null;
+  status: string;
+  totalResults: number;
+  articles?: Array<ArticlesEntity> | null;
 }
 export interface ArticlesEntity {
-    source: Source;
-    author?: string | null;
-    title: string;
-    description: string;
-    url: string;
-    urlToImage?: string | null;
-    publishedAt: string;
-    content: string;
+  source: Source;
+  author?: string | null;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage?: string | null;
+  publishedAt: string;
+  content: string;
 }
 export interface Source {
-    id?: null;
-    name: string;
+  id?: null;
+  name: string;
 }
